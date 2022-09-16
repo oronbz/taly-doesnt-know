3972b9a4b9f4732c1ca21483732d0b1c3a1d875b
Oron Ben Zvi <oronbz@gmail.com>
2022-09-16 21:24:55 +0300

    Initial Commit

---
+++ TalyDoesntKnowV1/TalyDoesntKnowV1App.swift
@@ -0,0 +1,17 @@
+//
+//  TalyDoesntKnowV1App.swift
+//  TalyDoesntKnowV1
+//
+//  Created by Oron Ben Zvi on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TalyDoesntKnowV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
