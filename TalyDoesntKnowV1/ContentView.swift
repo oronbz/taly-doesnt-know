f4ff213cbb233d29d6839fcd261f2e22921ff33a
Oron Ben Zvi <oronbz@gmail.com>
2022-09-16 21:35:03 +0300

    3. List with hardcoded text

--- TalyDoesntKnowV1/ContentView.swift
+++ TalyDoesntKnowV1/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("What is **Application**?")
+        List {
+            Text("What is **404**?")
+            Text("What is **API**?")
+            Text("What is **Application**?")
+        }
     }
 }
 
